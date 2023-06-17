4ab6ecb9df1f352ec238ba69738d30c1338ae3e7
hessdale <hessdale96@gmail.com>
2023-06-17 17:19:23 -0600

    changed get profile to args instead of json in calling procedure

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def new_client():
 def login():
     try:
         error = dbhelper.check_endpoint_info(request.json,["username","password"])
-        # if it has req info "username","email","password","image_url","bio" then returns none
+        # if it has req info "username","password" then returns none
         if(error != None):
             return make_response(jsonify(error),400)
         token = uuid.uuid4().hex
@@ -39,7 +39,7 @@ def login():
                 [request.json.get("username"),request.json.get("password"),token])
         # if results come back as a list jsonify results
         if(type(results) == list):
-            return make_response(jsonify(results),200)           
+            return make_response(jsonify(results),200)         
         else:
             return make_response('sorry something went wrong',500)
     # some except blocks with possible errors
@@ -54,11 +54,11 @@ def login():
 def delete_token():
     try:
         error = dbhelper.check_endpoint_info(request.json,["token"])
-        # if it has req info "username","email","password","image_url","bio" then returns none
+        # if it has req info "token" then returns none
         if(error != None):
             return make_response(jsonify(error),400)
         results = dbhelper.run_procedure('call delete_token(?)',[request.json.get("token")])
-        # if results come back as a list jsonify results
+        # if results come back as a None make response with success message results
         if(results == None):
             return make_response('deletion successful',200)
         else:
@@ -75,11 +75,11 @@ def delete_token():
 @app.get('/api/client')
 def get_profile():
     try:
-        error = dbhelper.check_endpoint_info(request.json,["token"])
-        # if it has req info "username","email","password","image_url","bio" then returns none
+        error = dbhelper.check_endpoint_info(request.args,["token"])
+        # if it has req info "token" then returns none
         if(error != None):
             return make_response(jsonify(error),400)
-        results = dbhelper.run_procedure('call get_profile(?)',[request.json.get("token")])
+        results = dbhelper.run_procedure('call get_profile(?)',[request.args.get("token")])
         # if results come back as a list jsonify results
         if(type(results) == list):
             return make_response(jsonify(results),200)
